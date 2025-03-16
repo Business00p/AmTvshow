b31a071fe0dbea53afe47d405c3ccf9e873814f6
Tv show <businesskaro0565@gmail.com>
2025-03-16 20:56:52 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -113,7 +113,8 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>📂 File Name: {file_name}\n 📑 sɪᴢᴇ : {file_size} \n <blockquote> 𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/publictvshow">𝗧𝘃 𝘀𝗵𝗼𝘄 𝙊𝙧𝙞𝙜𝙞𝙣𝙖𝙡𝙨</a>」</blockquote> </b>""" 
+    FILE_CAPTION = """<b>📂File Name: {file_name}
+    <b> 📑 sɪᴢᴇ : {file_size} \n <blockquote> 𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/publictvshow">𝗧𝘃 𝘀𝗵𝗼𝘄 𝙊𝙧𝙞𝙜𝙞𝙣𝙖𝙡𝙨</a>」</blockquote> </b>""" 
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
