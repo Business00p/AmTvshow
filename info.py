e36ba7e430b4c2e5ff54d5a6da043406aed505bb
Tv show <businesskaro0565@gmail.com>
2025-06-12 20:23:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -44,10 +44,10 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/') #Support group link 
 #---------------------------------------------------------------
 IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/publictvshow/51")
+TUTORIAL = environ.get("TUTORIAL", "https://t.me/publictvshow/61")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
-SHORTENER_API = environ.get("SHORTENER_API", "")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", '')
+SHORTENER_API = environ.get("SHORTENER_API", "83890476805372e11136f2fc25b26ad0d17e1954")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'pocolinks.com')
 SHORTENER_API2 = environ.get("SHORTENER_API2", "")
 SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", '')
 SHORTENER_API3 = environ.get("SHORTENER_API3", "")
